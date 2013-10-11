fbdc6c3295fee2bfe196eeb9b12858839227578a
Sam <sami.mirimiri@live.com>
2013-10-11 15:24:22 -0400

    Fixing Migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @player = Player.paginate(:page => 1, :per_page=> 2)
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
